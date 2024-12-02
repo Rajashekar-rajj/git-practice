38cd6150378543929a21d79fce1c289eb77dd62f
Rajashekar-rajj <rajashekarmekala2468@gmail.com>
2024-12-02 20:53:38 +0530

    Update 03 variable.sh

---
+++ 03 variable.sh
@@ -0,0 +1,8 @@
+person1 = Ramesh
+person2 = Suresh
+
+echo "${person1} Hi.. ${person2} How  are you..!!"
+echo "${peson2} Helo.. mr.${person2} I fine thank you, nice to meet you"
+echo "${person1} How is your work going on.. "
+echo "${person2} it is going good..."
+echo "All is good"
